5a3506d6ce004a49b967e6776d1cad71b42fea63
Michal <bondar4@o2.pl>
2019-03-16 12:48:06 +0100

    dodano nowe funkcje

--- src/com/company/QuestionsController.java
+++ src/com/company/QuestionsController.java
@@ -9,9 +9,9 @@ public class QuestionsController {
         String randomQ;
         String answer;
         String exit = "Wyjście";
-        int counter = 0;
+        int counter = 1;
         int r;
-        int lifebuoy =0; // w sytuacji podbramkowej, gdy gracz będzie zagrożony przegraną i nie będzie znał odpowiedzi na pytanie, będzie miał możliwość wpisania wybrania opcji "skip", co spowoduje przeskok do następnego pytania bez utraty punktu. Koło ratunkowe może zostać użyte tylko 3 razy.
+        int lifebuoy = 0; // w sytuacji podbramkowej, gdy gracz będzie zagrożony przegraną i nie będzie znał odpowiedzi na pytanie, będzie miał możliwość wpisania wybrania opcji "skip", co spowoduje przeskok do następnego pytania bez utraty punktu. Koło ratunkowe może zostać użyte tylko 3 razy.
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
@@ -36,6 +36,9 @@ public class QuestionsController {
                 } else {
                     System.out.println("Zła odpowiedź !");                         // jeśli uda się wzbudzić w graczu presję i zmieni zdanie, niestety traci punkt
                     counter--;
+                    if (counter == 0){
+                        System.out.println("Straciłes wszystkie punkty ! Przegrałeś !");
+                    }
                 }
             } else if (questionsDB.getQuestionsDb().containsValue(answer)) {
                 System.out.println("Prawidłowa odpowiedź !");                       // gdy warunek dodatkowego pytania nie zostanie spełniony, a gracz odpowie dobrze, naturalnie otrzymuje punkt
@@ -44,21 +47,38 @@ public class QuestionsController {
                 System.out.println("Zakończyłeś grę. Udało Ci się zdobyc " + counter + " punktów");         // konczy grę i pokazuję liczbę punktów, jakie udało nam się zdobyć
 
 
+            } else if (answer.equals("skip")){      // komenda koła ratunkowego
+                lifebuoy++;
+                if (lifebuoy <= 3){
+                    System.out.println("Pominięcie pytania ! Wykorzystałeś już: "+lifebuoy+" z 3 przysługujących Ci kół ratunkowych");  // jeśli gracz ma dostępne koła, może pominąć pytanie na które nie zna odpowiedzi bez konwekwencji
+                }else if (lifebuoy > 3){
+                    counter--;
+                    System.out.println("Nie masz już dostępnych kół ratunkowych ! Tracisz punkt !");// jeśli gracz wykorzystał już wszystkie koła, traci punkt
+                    if (counter == 0){
+                        System.out.println("Straciłes wszystkie punkty ! Przegrałeś !");
+                    }
+                }
             } else if (!questionsDB.getQuestionsDb().containsValue(answer) && r % 2 == 0) {         // jw. sprawdza, czy warunek do wyświetlenia dodatkowego pytania został spełniony, przy udzieleniu złej odpowiedzi pytanie dodatkowe pełni raczej funkcję "koła ratunkowego", choć gracz nie jest o tym świadomy.
                 System.out.println("Jesteś pewien ? Zastanów się jeszcze raz :)");
                 answer = scanner.nextLine();
                 if (!questionsDB.getQuestionsDb().containsValue(answer)) {
                     System.out.println("Zła odpowiedź !");                                  // jeśli gracz mimo pytania pomocniczego odpowie źle, naturalnie traci punkt
                     counter--;
+                    if (counter == 0){
+                        System.out.println("Straciłes wszystkie punkty ! Przegrałeś !");
+                    }
                 } else if (questionsDB.getQuestionsDb().containsValue(answer)) {
                     System.out.println("Odpowiedziałeś dobrze za drugim razem. Udało Ci się nie stracić punktu, ale też żadnego nie zyskujesz !");     // jeśli gracz poprawi swoją odpowiedź na poprawną nie traci punktu, ale też żadnego nie zyskuje
                 }
             }else if (!questionsDB.getQuestionsDb().containsValue(answer)) {
                     System.out.println("Zła odpowiedź !");                          // jeśli warunek pytania pomocniczego nie zostanie spełniony, a gracz udzielił złej odpowiedzi, traci punkt
                     counter--;
+                if (counter == 0){
+                    System.out.println("Straciłes wszystkie punkty ! Przegrałeś !");
                 }
+            }
 
-            }while (!answer.equals(exit));                     // gra trwa do momentu aż gracz sam ją zakończy
+            }while (!answer.equals(exit) && counter > 0);                     // gra trwa do momentu aż gracz sam ją zakończy
         }
     }
 
